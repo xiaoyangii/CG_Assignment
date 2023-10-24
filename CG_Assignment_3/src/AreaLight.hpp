85d0ccb183f550cfd74949ec405b5ffe3521f4e3
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-10-24 23:13:17 +0800

    fix：8 bugs

--- CG_Assignment_3/src/AreaLight.hpp
+++ CG_Assignment_3/src/AreaLight.hpp
@@ -1,5 +1,5 @@
 //
-// Created by GÃ¶ksu GÃ¼vendiren on 2019-05-14.
+// Created by G?ksu G¨¹vendiren on 2019-05-14.
 //
 
 #pragma once
@@ -10,24 +10,24 @@
 
 class AreaLight : public Light
 {
-public:
-    AreaLight(const Vector3f &p, const Vector3f &i) : Light(p, i)
-    {
-        normal = Vector3f(0, -1, 0);
-        u = Vector3f(1, 0, 0);
-        v = Vector3f(0, 0, 1);
-        length = 100;
-    }
+	public:
+		AreaLight(const Vector3f &p, const Vector3f &i) : Light(p, i)
+		{
+			normal = Vector3f(0, -1, 0);
+			u = Vector3f(1, 0, 0);
+			v = Vector3f(0, 0, 1);
+			length = 100;
+		}
 
-    Vector3f SamplePoint() const
-    {
-        auto random_u = get_random_float();
-        auto random_v = get_random_float();
-        return position + random_u * u + random_v * v;
-    }
+		Vector3f SamplePoint() const
+		{
+			auto random_u = get_random_float();
+			auto random_v = get_random_float();
+			return position + random_u * u + random_v * v;
+		}
 
-    float length;
-    Vector3f normal;
-    Vector3f u;
-    Vector3f v;
+		float length;
+		Vector3f normal;
+		Vector3f u;
+		Vector3f v;
 };
